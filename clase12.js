94812bd9847c8b253ac5321c30707cf334748b01
Julieta Ciraolo <julycir@gmail.com>
2020-05-06 13:04:23 -0300

    Add files via upload

---
+++ clase12.js
@@ -0,0 +1,10 @@
+/* Definir una función llamada imprimirNumeros que reciba como parámetro un número
+e imprima en consola todos los números desde el 1 hastsa el número del parámetro. */
+
+function imprimirNumeros(numero) {
+	for (i = 1; i <= numero; i++) {
+	console.log(i)
+	}
+}
+
+
