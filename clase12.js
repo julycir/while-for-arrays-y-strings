e9b99e3413c0b5a629d7a37d39229d83f5207889
Julieta Ciraolo <julycir@gmail.com>
2020-05-06 14:26:44 -0300

    Add files via upload

--- clase12.js
+++ clase12.js
@@ -3,8 +3,17 @@ e imprima en consola todos los números desde el 1 hastsa el número del paráme
 
 function imprimirNumeros(numero) {
 	for (i = 1; i <= numero; i++) {
-	console.log(i)
+		console.log(i)
 	}
 }
 
+/* Definir una función llamada imprimirOtrosNumeros que reciba dos parámetros: un
+número desde y un número hasta. En el caso que el número desde sea menor al
+número hasta, debe imprimir en consola todos los números que están entre el primer
+parámetro y el segundo. */
 
+function imprimirOtrosNumeros(numeroDesde, numeroHasta) {
+	for (i = numeroDesde; i <=numeroHasta; i++) {
+		console.log(i)
+	}
+}
